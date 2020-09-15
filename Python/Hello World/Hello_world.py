faacd06057c74f1911020c5c5215e69e70e01043
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-15 17:42:20 +0300

    Hello World

---
+++ Python/Hello World/Hello_world.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+
+# Variables
+message = "Hello World"
+print(message, "\n")
+
+character_name = "Bryan"
+character_age = 70
+
+print(f"There was once a man named {character_name}. ")
+print(f"He was {character_age} years old. ")
+print(f"He liked the name {character_name}. ")
+print(f"But he never liked being {character_age}.\n")
+
+message_1 = "The manager has just reported to the C.E.O"
+print(message_1, "\n")
+message = message_1
+print(message)
+print(message, "|", message_1)
+
+print("This world is not my home")
